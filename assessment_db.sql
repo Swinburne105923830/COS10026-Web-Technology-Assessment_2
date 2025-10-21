365d33c7d9c048229e88bd52adb5612311edd96a
Damian <105923830@student.swin.edu.au>
2025-10-21 13:03:15 +1100

    added sql database file

---
+++ assessment_db.sql
@@ -0,0 +1,44 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Oct 21, 2025 at 04:02 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `assessment_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `jobs_list`
+--
+
+CREATE TABLE `jobs_list` (
+  `title` text NOT NULL,
+  `refrence_no` text NOT NULL,
+  `description` text NOT NULL,
+  `pay_min` int(11) NOT NULL,
+  `pay_max` int(11) NOT NULL,
+  `responsibility` text NOT NULL,
+  `qualification` text NOT NULL,
+  `report_line` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
