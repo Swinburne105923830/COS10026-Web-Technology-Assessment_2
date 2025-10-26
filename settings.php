5a336c8fdd6bd0a939242d84b3fcfa2c3c26ed76
105747580 <105747580@student.swin.edu.au>
2025-10-27 00:29:56 +1100

    added conn allowing connection the the database in settings.php

--- settings.php
+++ settings.php
@@ -2,4 +2,11 @@
 $host = "localhost";         
 $user = "root";          
 $pwd = "";              
-$sql_db = "assessment_db";  
+$sql_db = "assessment_db"; 
+
+$conn = mysqli_connect($host, $user, $pwd, $sql_db);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
