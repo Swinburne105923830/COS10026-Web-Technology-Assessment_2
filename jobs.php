bb2dac57bacd7b31e77abb8a93fde8b378c88da8
Damian <105923830@student.swin.edu.au>
2025-10-27 21:55:19 +1100

    Revert to using jobs_list

--- jobs.php
+++ jobs.php
@@ -64,7 +64,7 @@
             <?php include "settings.php";
             $dbconn = mysqli_connect($host, $user, $pwd, $sql_db);
             if ($dbconn) {
-                $query = "SELECT * FROM jobs";
+                $query = "SELECT * FROM jobs_list";
                 $result = mysqli_query($dbconn, $query);
                 if (!$result) {
                     echo "<p>There are no jobs to display.";
